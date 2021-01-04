6dca3093729d4c814f18f4c5f91159461e824e79
miari <maryna.boroday@gmail.com>
2021-01-04 23:22:04 +0100

    Logback file  appender was added

--- src/main/java/com/boroday/movieland/dao/jdbc/mapper/MovieRowMapper.java
+++ src/main/java/com/boroday/movieland/dao/jdbc/mapper/MovieRowMapper.java
@@ -1,12 +1,13 @@
 package com.boroday.movieland.dao.jdbc.mapper;
 
 import com.boroday.movieland.entity.Movie;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
+@Slf4j
 public class MovieRowMapper implements RowMapper<Movie> {
 
     public Movie mapRow(ResultSet resultSet, int countOfRecords) throws SQLException {
@@ -19,6 +20,17 @@ public class MovieRowMapper implements RowMapper<Movie> {
         movie.setRating(resultSet.getDouble("rating"));
         movie.setPrice(resultSet.getDouble("price"));
         movie.setPicturePath(resultSet.getString("picturePath"));
+        if (movie.getId() == 0 ||
+                movie.getNameRu() == null ||
+                movie.getNameEn() == null ||
+                movie.getYearOfProduction() == 0 ||
+                movie.getDescription() == null ||
+                movie.getRating() == 0 ||
+                movie.getPrice() == 0 ||
+                movie.getPicturePath() == null ) {
+            log.info("Incorrect data for movie mapping");
+            throw new RuntimeException("Incorrect data for movie mapping");
+        }
         return movie;
     }
 }
